16a6e946bb38a62f099fe41d54ea9889aa539a24
bingsennn <e0318666@u.nus.edu>
2021-03-29 17:24:29 +0800

    account CRD

--- Medichain_Backend/service/account.service.js
+++ Medichain_Backend/service/account.service.js
@@ -1,14 +1,61 @@
 const Account = require('../model/account.model')
 const SystemAdmin = require('../model/systemAdmin.model')
+const Policyholder = require('../model/policyholder.model')
+const Insurer = require('../model/Insurer.model')
 
-async function createSystemAdmin (systemAdmin) {
+async function createSystemAdmin(systemAdmin) {
 
     return SystemAdmin.create(systemAdmin)
-    .catch((err) => {
-        throw err
-    })
+        .catch((err) => {
+            throw err
+        })
+}
+
+async function createPolicyholder(newPolicyholder) {
+
+    return Policyholder.create(newPolicyholder)
+        .catch((err) => {
+            throw err
+        })
+}
+
+async function createInsurer(newInsurer) {
+
+    return Insurer.create(newInsurer)
+        .catch((err) => {
+            throw err
+        })
+}
+
+async function readAllAccounts() {
+    return Account.find()
+        .catch((err) => {
+            throw err
+        })
+}
+
+async function readAccount(username) {
+    return Account.findOne({username: username})
+        .catch((err) => {
+            throw err
+        })
+}
+
+async function deleteAccountByUsername(username) {
+    return Account.findOneAndDelete(
+        {
+            username: username
+        })
+        .catch((err) => {
+            throw err
+        })
 }
 
 module.exports = {
-    createSystemAdmin
+    createSystemAdmin,
+    createPolicyholder,
+    createInsurer,
+    readAllAccounts,
+    readAccount,
+    deleteAccountByUsername
 }
