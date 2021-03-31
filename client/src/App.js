3199177edd3bb70b5745a8e4fb4b6654bf0270be
Tze Ming <e0311042@u.nus.edu>
2021-03-31 23:35:49 +0800

    init homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Test from "./components/Test";
 import PrivateRoute from "./components/PrivateRoute";
+import HomePage from "./components/HomePage";
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/medichain/login" component={Login} />
+        <Route exact path="/emrx/login" component={Login} />
         <PrivateRoute path="/test" component={Test} />
+        <Route component={HomePage} />
       </Switch>
     );
   }
