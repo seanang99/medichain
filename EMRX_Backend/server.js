1a64ac3174b02912ccdbb14e40ae5fad798d5444
kojiale <kojiale@gmail.com>
2021-04-08 23:12:18 +0800

    db init for EMRX

--- EMRX_Backend/server.js
+++ EMRX_Backend/server.js
@@ -3,18 +3,24 @@ const app = express();
 const cors = require('cors');
 const port = 3002;
 const mongoose = require('mongoose');
+const dbInit = require('./dbInit');
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
 
-const uri = "mongodb+srv://admin:passwordAdmin@cluster0.z77gk.gcp.mongodb.net/emrx?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:passwordAdmin@cluster0.z77gk.gcp.mongodb.net/emrxTestInit?retryWrites=true&w=majority";
 app.use(cors());
 app.use(express.json());
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
 const dbConnection = mongoose.connection;
+
 dbConnection.once('open', () => {
     console.log("EMRX Mongoose DB Connection established Successfully!")
+    // DB Init - Can be commented out if you dont want to DB Init for every changes made in backend codes
+    dbInit.dbInit()
+        .then(() => console.log('Data Init Done!'))
+        .catch(err => console.log('DB Init: ' + err))
 });
 
 const medicalInstitutionAccountRouter = require('./route/medicalInstitutionAccount.route');
@@ -26,3 +32,5 @@ app.use('/medicalInstitutionAccount', medicalInstitutionAccountRouter);
 app.use('/patient', patientRouter);
 app.use('/medicalRecord', medicalRecordRouter);
 app.use('/token', tokenRouter);
+
+module.exports = exports = mongoose;
